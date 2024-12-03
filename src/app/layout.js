fdc6f31cf89155e1569ca0114c7ef9a597f386c6
SulioDev <suliodevelopers@gmail.com>
2024-12-03 13:46:23 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className="bg-black text-white">
+    <head>
+    <meta name="facebook-domain-verification" content="domwcotoevocvy9k2s82nabc8fz87j" />
+    </head>
       <body>
         {children}
       </body>
