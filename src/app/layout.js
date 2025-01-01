c485f238d73318e3f2a0f731cb6a6a300a967cbc
SulioDev <suliodevelopers@gmail.com>
2025-01-01 15:03:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import "./globals.css";
 
-export const metadata = {
+const metadata = {
   title: "Sulio Art",
   description: "Sulio Art | Artist AI Chatbot",
   other: {
     'facebook-domain-verification': 'domwcotoevocvy9k2s82nabc8fz87j'
   }
 };
+
+export { metadata };
 
 export default function RootLayout({ children }) {
   return (
